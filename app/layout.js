b2bd87dbbfd99fa67926f65ede2df8b0b164c35c
Spoilerboy555 <b.nieuwenhuis@flexxinvestments.nl>
2026-03-18 16:09:39 +0100

    Add files via upload

---
+++ app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: 'De Pandgevers Tool',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="nl">
+      <body>{children}</body>
+    </html>
+  );
+}
